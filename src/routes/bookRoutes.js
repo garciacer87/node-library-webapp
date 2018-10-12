29a8356d303a46558d2d323ff2ccdc786bd448b8
Carlos <garciacer87@gmail.com>
2018-10-12 02:58:23 -0400

    First commit!

---
+++ src/routes/bookRoutes.js
@@ -0,0 +1,25 @@
+/**
+ * Created by cgarcia on 10/9/18.
+ */
+
+const express = require('express');
+const bookController = require('../controllers/bookController');
+const bookService = require('../services/goodreadsServices');
+
+const bookRouter = express.Router();
+
+function router(nav) {
+  const { getIndex, getById, middleware } = bookController(bookService, nav);
+
+  bookRouter.use(middleware);
+
+  bookRouter.route('/')
+    .get(getIndex);
+
+  bookRouter.route('/:id')
+    .get(getById);
+
+  return bookRouter;
+}
+
+module.exports = router;
